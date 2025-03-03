5797f5ad9518cbc69e5984efd3e2bf93c3ba2224
KodderCraft <codygaamez@gmail.com>
2025-03-03 13:08:58 -0500

    version_2_navbar_

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from "./logo_tranp.png";
-const Navbar = () => {
+import { useState } from "react";
+
     const link_nav= [
         {   id : 1,
             nombre : "Home",
@@ -40,16 +41,20 @@ const Navbar = () => {
             nombre : "tiktok",
             icono : "bi bi-tiktok",
             link: "https://www.tiktok.com/@connley.abg.ec?_t=ZM-8uCboXlVnEt&_r=1"
-           },
-           
-           
-                   
+           },            
     ]
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const menu_mobil = () =>{
+        setOpen (!open)
+    };
+    
 
   return (
-        <nav className="relative md:fixed h-auto  w-full  sm:flex justify-between items-center py-3 font-medium font-ubuntu px-5 md:px-25 lg:px-45 xl:px-60 bg-azul-100/97 backdrop-blur-xs">
+        <nav className=" fixed h-auto  w-full  flex justify-between items-center py-3 font-medium font-ubuntu px-8 md:px-25 lg:px-45 xl:px-60 bg-azul-100/97 backdrop-blur-xs">
         
-
+        {/* menu para escritorio */}
 
         <img  src={logo} alt="logo"  className=" h-[60px] w-auto transition-all duration-300  inline-block hover:scale-105  select-none"/>
 
@@ -57,15 +62,20 @@ const Navbar = () => {
                 {link_nav.map((datos) => (<li key={datos.id}><a /*target="__blank"*/ href={datos.link} 
                 className="text-amber-50  transition-all duration-300   inline-block hover:scale-110 ">{datos.nombre}</a></li>)
                 )}
-            </ul>
-
+            </ul>  
          
+         {/* BOTON menu para mobil */}
+        <button onClick={menu_mobil} className="sm:hidden scale-y-120 text-[40px]  text-amber-50   cursor-pointer " >{open ? (<i class="bi bi-list  transition-all duration-400"></i>) : (<i className="bi bi-x "></i>)}
+            </button>
+        
+        {/* menu mobil  */}
+        <div className={` z-0 absolute top-[100%] right-0  w-full h-screen flex justify-end transition-colors  ${open? "bg-transparent": "bg-azul-100/15 cursor-pointer"} ` } onClick={()=>setOpen(true)}>
+            <div onClick={(e) => e.stopPropagation()} className={ `z-10 cursor-default absolute  flex sm:hidden flex-col justify-between  bg-azul-100/90  h-[88vh] w-3xs transition-all duration-300   ${open ? ("left-[-350px] ") : (" left-[0] ")}`} >
             
-         <div className="flex flex-col justify-between sm:hidden bg-azul-100/90 absolute top-[100%] h-[88vh] w-3xs right-0 ">
             <ul>
                 {
                     link_nav.map( (datos) => (
-                        <li key={datos.id} className=" text-amber-50 transition-colors duration-200 hover:bg-hoverazul/10 w-full  px-6 py-4">
+                        <li key={datos.id} className=" cursor-pointer text-amber-50 transition-colors duration-200 hover:bg-hoverazul/10 w-full  px-6 py-4">
                             <a href={datos.link} >{datos.nombre}</a>
                         </li>                            
                     ))
@@ -83,7 +93,9 @@ const Navbar = () => {
                     )
                 }
             </ul>
+            </div>
         </div>
+        
         </nav>
     
   )
